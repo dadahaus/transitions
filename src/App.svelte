e05f5c4312b29ca9613d93c872f955f6d6f08b1f
kosme <casadadaista@gmail>
2023-12-26 17:50:14 +1100

    second

--- src/App.svelte
+++ src/App.svelte
@@ -3,9 +3,55 @@
 </script>
 
 <main>
-  <h1>hello</h1>
+
+
+
+
+  <h1>Vanilla css Scroll</h1>
+  <article>
+    <h2>Nice features</h2>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem, ipsum omnis earum quod accusantium rerum, aspernatur error beatae dolores ab, molestias quia fuga? Maiores animi natus itaque! Perferendis, suscipit odit?</p>
+    <ul>
+      <li>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis ab recusandae inventore odit aspernatur. Pariatur repellat odit rerum! Molestiae ipsa eius officiis! Quo, sint libero sapiente repudiandae aliquid at reiciendis?
+      </li>
+      <li>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis ab recusandae inventore odit aspernatur. Pariatur repellat odit rerum! Molestiae ipsa eius officiis! Quo, sint libero sapiente repudiandae aliquid at reiciendis?
+      </li>
+    </ul>
+    <div>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum, quos ab libero reprehenderit velit eum nam inventore tenetur? Accusantium esse a dolorum ducimus deserunt nihil doloribus consectetur, sint reiciendis debitis.</p>
+    </div>    
+
+    <div><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti exercitationem sit dolores aliquid in ea. Sed in aliquam, quibusdam quas dolorum est amet sit iure, quisquam, ducimus quaerat assumenda repudiandae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla et a suscipit voluptate, omnis esse vitae eos ex repellat ad reprehenderit ut, vel, facilis quod consequuntur. Nemo accusamus suscipit vitae?</p></div>
+    <div><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla et a suscipit voluptate, omnis esse vitae eos ex repellat ad reprehenderit ut, vel, facilis quod consequuntur. Nemo accusamus suscipit vitae?</p></div>
+    <div><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla et a suscipit voluptate, omnis esse vitae eos ex repellat ad reprehenderit ut, vel, facilis quod consequuntur. Nemo accusamus suscipit vitae?</p></div>
+    <figure>
+      <img src="src/assets/1.png" alt="pic">
+      <figcaption>Some artsy stuff</figcaption>
+    </figure>
+  </article>
+
+
+
 </main>
 
-<style>
-
+<style lang="scss">
+  :root {
+    font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+    line-height: 1.5;
+    font-weight: 400;
+  
+    color-scheme: light dark;
+    color: hsla(165, 60%, 17%, 0.87);
+    background-color: hsl(158, 89%, 89%);
+  
+    font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  h1{
+    color: hsl(180, 79%, 15%);
+  }
 </style>
