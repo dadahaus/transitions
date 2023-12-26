fc06f6a00621c4ca53fda9d8e4af25a8da1410b6
kosme <casadadaista@gmail>
2023-12-26 18:24:55 +1100

    new additions

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,69 @@
 <script>
-
 </script>
 
 <main>
-
-
-
-
   <h1>Vanilla css Scroll</h1>
   <article>
     <h2>Nice features</h2>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem, ipsum omnis earum quod accusantium rerum, aspernatur error beatae dolores ab, molestias quia fuga? Maiores animi natus itaque! Perferendis, suscipit odit?</p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem, ipsum omnis
+      earum quod accusantium rerum, aspernatur error beatae dolores ab,
+      molestias quia fuga? Maiores animi natus itaque! Perferendis, suscipit
+      odit?
+    </p>
     <ul>
       <li>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis ab recusandae inventore odit aspernatur. Pariatur repellat odit rerum! Molestiae ipsa eius officiis! Quo, sint libero sapiente repudiandae aliquid at reiciendis?
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis ab
+        recusandae inventore odit aspernatur. Pariatur repellat odit rerum!
+        Molestiae ipsa eius officiis! Quo, sint libero sapiente repudiandae
+        aliquid at reiciendis?
       </li>
       <li>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis ab recusandae inventore odit aspernatur. Pariatur repellat odit rerum! Molestiae ipsa eius officiis! Quo, sint libero sapiente repudiandae aliquid at reiciendis?
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis ab
+        recusandae inventore odit aspernatur. Pariatur repellat odit rerum!
+        Molestiae ipsa eius officiis! Quo, sint libero sapiente repudiandae
+        aliquid at reiciendis?
       </li>
     </ul>
     <div>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum, quos ab libero reprehenderit velit eum nam inventore tenetur? Accusantium esse a dolorum ducimus deserunt nihil doloribus consectetur, sint reiciendis debitis.</p>
-    </div>    
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum, quos ab
+        libero reprehenderit velit eum nam inventore tenetur? Accusantium esse a
+        dolorum ducimus deserunt nihil doloribus consectetur, sint reiciendis
+        debitis.
+      </p>
+    </div>
 
-    <div><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti exercitationem sit dolores aliquid in ea. Sed in aliquam, quibusdam quas dolorum est amet sit iure, quisquam, ducimus quaerat assumenda repudiandae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla et a suscipit voluptate, omnis esse vitae eos ex repellat ad reprehenderit ut, vel, facilis quod consequuntur. Nemo accusamus suscipit vitae?</p></div>
-    <div><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla et a suscipit voluptate, omnis esse vitae eos ex repellat ad reprehenderit ut, vel, facilis quod consequuntur. Nemo accusamus suscipit vitae?</p></div>
-    <div><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla et a suscipit voluptate, omnis esse vitae eos ex repellat ad reprehenderit ut, vel, facilis quod consequuntur. Nemo accusamus suscipit vitae?</p></div>
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+        exercitationem sit dolores aliquid in ea. Sed in aliquam, quibusdam quas
+        dolorum est amet sit iure, quisquam, ducimus quaerat assumenda
+        repudiandae. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Nulla et a suscipit voluptate, omnis esse vitae eos ex repellat ad
+        reprehenderit ut, vel, facilis quod consequuntur. Nemo accusamus
+        suscipit vitae?
+      </p>
+    </div>
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla et a
+        suscipit voluptate, omnis esse vitae eos ex repellat ad reprehenderit
+        ut, vel, facilis quod consequuntur. Nemo accusamus suscipit vitae?
+      </p>
+    </div>
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla et a
+        suscipit voluptate, omnis esse vitae eos ex repellat ad reprehenderit
+        ut, vel, facilis quod consequuntur. Nemo accusamus suscipit vitae?
+      </p>
+    </div>
     <figure>
-      <img src="src/assets/1.png" alt="pic">
+      <img src="src/assets/1.png" alt="pic" />
       <figcaption>Some artsy stuff</figcaption>
     </figure>
   </article>
-
-
-
 </main>
 
 <style lang="scss">
@@ -41,17 +71,48 @@
     font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
     line-height: 1.5;
     font-weight: 400;
-  
+    font-size: clamp(0.125rem, 2vw, 4.5rem);
+
     color-scheme: light dark;
     color: hsla(165, 60%, 17%, 0.87);
     background-color: hsl(158, 89%, 89%);
-  
+
     font-synthesis: none;
     text-rendering: optimizeLegibility;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
-  h1{
+  h1 {
+    margin: 20rem 0rem;
     color: hsl(180, 79%, 15%);
   }
+
+  article {
+    max-width: 50ch;
+  }
+  
+  p {
+      animation: fade linear;
+      animation-timeline: view();
+    }
+    @keyframes fade {
+      from {
+        
+      }
+      to {
+        
+      }
+      0% {
+        opacity: 0;
+      }
+      60% {
+        opacity: 1;
+      }
+      80% {
+        opacity: 0;
+      }
+      100% {
+        opacity: 0;
+      }
+    }
 </style>
